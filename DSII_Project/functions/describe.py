c9377388b273fb457d5f1e141219673a6f30260a
EstefaniaOchoa73 <ayelenestefaniaochoa@gmail.com>
2025-06-14 21:21:22 -0300

    Modificacion en Pre Entega

---
+++ DSII_Project/functions/describe.py
@@ -0,0 +1,4 @@
+def decripcionDF(df):
+    print("--DECRIBE--")
+    print("\n")
+    print(df.describe())
--- DSII_Project/functions/info.py
+++ DSII_Project/functions/info.py
@@ -3,6 +3,3 @@ def infoDataFrame (df):
     print(df.info())
     print("\n-- HEAD --")
     print(df.head())
-    print("\n-- DESCRIBRE --")
-    print(df.describe())
-    print("\nCantidad de Filas: ",len(df))   
